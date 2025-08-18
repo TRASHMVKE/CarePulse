3466017f2e92ba0a6e4552509a1eb4e61f79e433
Michael Rodriguez <michaelsmidrodriguez31@gmail.com>
2025-08-18 02:24:56 -0400

    sdgdsg

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,15 @@
+// next.config.js
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {typescript:{
-  ignoreBuildErrors:true
-},
-eslint:{
-  ignoreDuringBuilds:true,
-}
+const nextConfig = {
+  typescript: {
+    // Solo para emergencias, ignora errores de tipado durante el build
+    ignoreBuildErrors: true
+  },
+  eslint: {
+    // Solo para emergencias, ignora errores de linting durante el build
+    ignoreDuringBuilds: true,
+  }
 };
 
+export default nextConfig;
